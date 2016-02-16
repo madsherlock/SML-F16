f06381d2be7e688be3bb74573b1a33f017e022b5
Mikael Westermann <mikael.westermann@gmail.com>
2016-02-16 08:05:19 +0100

    gitignore dammit

---
+++ Rcode/smooth.R
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------
+#load libraries
+#-------------------------------------------------------------
+library("EBImage")
+
+#-------------------------------------------------------------
+#Functions to be used in evaluation
+#-------------------------------------------------------------
+
+#inspiration for smoothing
+smoothAverage <- function(grayImg,k){
+  kernel <- matrix( 
+    1, # the data elements 
+    k,# number of rows 
+    k)
+  kernel <- kernel/k*k
+  #print(kernel)
+  #using r library for smoothing
+  smoothed <- filter2(grayImg, kernel)
+  return(smoothed)
+}
+
+#Gaussian blur: See gblur from EBImage
+# gblur(grayImg,sigma,radius = 2*ceiling(3*sigma)+1)
+
+smoothGaussian <- function(grayImg,sigma,k = 2*ceiling(3*sigma)+1){
+  return(gblur(grayImg,sigma,k))
+}
+
+
