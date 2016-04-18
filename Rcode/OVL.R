5010e6fa88d44e4dcf78b8bf804572ecb771a4b7
Keerthikan <kerat12@student.sdu.dk>
2016-04-18 15:49:31 +0200

    Weitzman's impl

--- Rcode/OVL.R
+++ Rcode/OVL.R
@@ -1,6 +1,7 @@
 #Overlapping Coefficient computer 
 #Source: http://stats.stackexchange.com/questions/12209/percentage-of-overlapping-regions-of-two-normal-distributions
 #Input x = number of obs, (mu1,sd_1) = (µ_1,std_1),(mu2,sd_2) = (µ_2,std_2) 
+#Weitzman's impl. 
 
 min.f1f2 <- function(x, mu1, mu2, sd1, sd2) {
   f1 <- dnorm(x, mean=mu1, sd=sd1)
